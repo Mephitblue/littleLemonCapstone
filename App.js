0ea208422f14aa9206e02de45ae4e191fb75db6f
Keith Lamond <keith.lamond@gmail.com>
2024-05-21 16:29:23 -0700

    Adding navigation

--- App.js
+++ App.js
@@ -1,19 +1,73 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Onboarding from "./screens/Onboarding";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+import SplashScreen from "./screens/SplashScreen";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+  const [state, setState] = useState({
+    isLoggedIn: false,
+    isLoading: true,
+    isOnboardingCompleted: false,
+  });
+
+  const checkIfOnboardingCompleted = async () => {
+    try {
+      const value = await AsyncStorage.getItem("isOnboardingCompleted");
+      if (value !== null) {
+        setState({
+          ...state,
+          isOnboardingCompleted: JSON.parse(value),
+          isLoading: false,
+        });
+      } else {
+        setState({
+          ...state,
+          isOnboardingCompleted: false,
+          isLoading: false,
+        });
+      }
+    } catch (e) {
+      setState({
+        ...state,
+        isLoggedIn: false,
+        isLoading: false,
+      });
+    }
+  };
+  useEffect(() => {
+    checkIfOnboardingCompleted();
+  }, []);
+
+  if (state.isLoading) {
+    return <SplashScreen />;
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Onboarding">
-        <Stack.Screen
-          name="Onboarding"
-          component={Onboarding}
-          hidename="true"
-        />
+      <Stack.Navigator>
+        {!state.isOnboardingCompleted ? (
+          <Stack.Screen
+            name="Onboarding"
+            component={Onboarding}
+            options={{
+              headerShown: false,
+            }}
+          />
+        ) : (
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              headerShown: false,
+            }}
+          />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
